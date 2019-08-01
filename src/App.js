00d51d5a7ce7474844321177b6e22e03bfac72d4
Armando Ruiz <armando.ruiz@industrie.co>
2019-08-02 09:41:27 +1000

    Consultando la API con axios y anadiendo funcionalidad para actualizar el state con useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  // state
+  const [frase, obtenerFrase] = useState({});
+  // console.log(frase);
+
+  const consultarApi = async () => {
+    const url = 'https://api.adviceslip.com/advice';
+    const resultado = await axios(url);
+
+    // console.log("resultado ", resultado.data.slip.advice); para ir comprobando el resultado.
+     obtenerFrase(resultado.data.slip);
+  }
+
+  useEffect(
+    () => {
+      consultarApi()
+    },[]
+  )
+
+  console.log("frase ", frase);
+
   return (
     <div className="App">
-      hello
+      La Frase va aqui
     </div>
   );
 }
